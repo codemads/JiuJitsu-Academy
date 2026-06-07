62fe83ae2bb6203bb1e344b82c08a97f86db9b53
madson10 <madsonsdj@hotmail.com>
2026-06-06 22:00:10 -0300

    feat: conexao supabase, login e register

--- js/login.js
+++ js/login.js
@@ -1,11 +1,24 @@
 const form = document.getElementById('loginForm');
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', async (event) => {
   event.preventDefault();
 
   const email = document.getElementById('email').value;
   const senha = document.getElementById('senha').value;
 
-  // Proximas feature:
-  // loginSupabase(email, senha);
+  const { data, error } = await supabaseClient.auth.signInWithPassword({
+    email,
+    password: senha,
+  });
+
+  console.log('Login realizado');
+  console.log(window.location.href);
+
+  if (error) {
+    console.error(error);
+    alert(error.message);
+    return;
+  }
+
+  window.location.href = '/pages/dashboard.html';
 });
--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
-const supabaseUrl = 'SUA_URL';
-const supabaseKey = 'SUA_ANON_KEY';
+const supabaseUrl = 'https://kmyxiedfzpsllycqjpfa.supabase.co';
 
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtteXhpZWRmenBzbGx5Y3FqcGZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3NzY0MjEsImV4cCI6MjA5NjM1MjQyMX0.dZfOh0FuAUhkqyCPfDsxxozphuYPiV3xf4bTHEdzlK8';
 
-export { supabase };
+window.supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
